f7f324a69008e19e17d8997d9c166e7fbbff7cea
Lior Kuperiu <lkuperiu@schubergphilis.com>
2018-10-17 16:20:38 +0200

    v2

--- Scripts/compile.sh
+++ Scripts/compile.sh
@@ -6,6 +6,7 @@ cd /app && sudo git clone https://github.com/kuperiu/ops_test.git
 if [ $? -ne 0 ]; then
     exit 1
 fi
+cd ops_test
 echo "availabilityZone=$availability_zone" > $prop_file
 ./mvnw package
 if [ $? -ne 0 ]; then
