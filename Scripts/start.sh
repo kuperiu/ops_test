e41de4bb30b3ba1397e581c071fc7f710d5264dc
Lior Kuperiu <lkuperiu@schubergphilis.com>
2018-10-17 18:53:52 +0200

    v2

--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Starting Spring Boot app'
 cd /app/ops_test/target
-java -jar suchapp-0.0.1-SNAPSHOT.jar
+java -jar suchapp-0.0.1-SNAPSHOT.jar  >/dev/null 2>&1 &
 if [ $? -ne 0 ]; then
     exit 1
 fi
