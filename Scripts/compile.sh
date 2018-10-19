0b8c9d59cbf7255633caa5e120500969fe2bdc0d
Lior Kuperiu <lkuperiu@schubergphilis.com>
2018-10-19 13:19:21 +0200

    v2

--- Scripts/compile.sh
+++ Scripts/compile.sh
@@ -6,10 +6,5 @@ cd /app && sudo git clone https://github.com/kuperiu/ops_test.git
 if [ $? -ne 0 ]; then
     exit 1
 fi
-cd ops_test
 echo "availabilityZone=$availability_zone" >> $prop_file
 echo "url=http://microservice-b-704943846.eu-central-1.elb.amazonaws.com/" >> $prop_file
-./mvnw package
-if [ $? -ne 0 ]; then
-    exit 1
-fi
--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Starting Spring Boot app'
-cd /app/ops_test/target
-java -jar suchapp-0.0.1-SNAPSHOT.jar  -Dspring.config.location=/tmp/application.properties >/dev/null 2>&1 &
+cd /app/ops_test
+spring-boot:run -Dspring.config.location=/tmp/application.properties >/dev/null 2>&1 &
 if [ $? -ne 0 ]; then
     exit 1
 fi
