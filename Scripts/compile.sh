26396591c19d5833b97d3328b574ca38346d72b9
Lior Kuperiu <lkuperiu@schubergphilis.com>
2018-10-19 09:59:19 +0200

    v2

--- Scripts/compile.sh
+++ Scripts/compile.sh
@@ -7,7 +7,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 cd ops_test
-echo "availabilityZone=$availability_zone" > $prop_file
+echo "availabilityZone=$availability_zone" >> $prop_file
+echo "url=http://microservice-B-704943846.eu-central-1.elb.amazonaws.com/" >> $prop_file
 ./mvnw package
 if [ $? -ne 0 ]; then
     exit 1
--- src/main/java/com/example/r42/SuchController.java
+++ src/main/java/com/example/r42/SuchController.java
@@ -3,14 +3,55 @@ package com.example.r42;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 @RestController
 public class SuchController {
 
 	@Value("${availabilityZone}") private String availabilityZone;
+	@Value("${url}") private String url;
 
 	@RequestMapping("/hello")
 	public String suchHello(){
-		return "hello " + availabilityZone;
+		String time = "";
+		try {
+			time = sendGET(url);
+		}
+		catch(IOException ie) {
+			System.out.println("fail");
+		}
+		return "hello " + availabilityZone + " " + time;
+	}
+
+	public static String sendGET(String url) throws IOException {
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			return response.toString();
+		} else {
+			System.out.println("GET request not worked");
+			return "lol";
+		}
+
 	}
 }
