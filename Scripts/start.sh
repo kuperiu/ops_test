bd27e63eb2646c7d09d84a7ed74e52923b4731d4
Lior Kuperiu <lkuperiu@schubergphilis.com>
2018-10-19 13:27:38 +0200

    v2

--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'Starting Spring Boot app'
 cd /app/ops_test
-spring-boot:run -Dspring.config.location=/tmp/application.properties >/dev/null 2>&1 &
+./mvnw spring-boot:run -Dspring.config.location=/tmp/application.properties >/dev/null 2>&1 &
 if [ $? -ne 0 ]; then
     exit 1
 fi
-sleep 60
+sleep 15
